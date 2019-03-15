b573ddc6cc30bb4135e2ff0c89727b63d6554938
Aleksey Belan <alexey.belan@spryker.com>
2019-03-15 21:03:33 +0200

    Glue: 5735 - Added SprykerInput componnet

--- src/application/pages/LoginPage/LoginForm/index.tsx
+++ src/application/pages/LoginPage/LoginForm/index.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
-import {
-    withStyles,
-    TextField,
-    Button,
-    Typography
-} from '@material-ui/core';
+import { withStyles, Button } from '@material-ui/core';
+import { SprykerInput } from '@application/components/UI/SprykerInput';
 import { ILoginFormProps as Props, ILoginFormState as State } from './types';
 import { FormEvent, InputChangeEvent } from '@interfaces/common';
 import { styles } from './styles';
 
-export class LoginFormBase extends React.Component<Props, State> {
+export class LoginFormComponent extends React.Component<Props, State> {
     public readonly state: State = {
         username: '',
         password: '',
@@ -20,80 +16,52 @@ export class LoginFormBase extends React.Component<Props, State> {
 
     protected handleSubmit = (event: FormEvent): void => {
         event.preventDefault();
-        if (!this.state.username || !this.state.password) {
+        const { username, password } = this.state;
+        if (Boolean(username) || Boolean(password)) {
             return null;
         }
-        const {username, password} = this.state;
-        const payload = {
-            username,
-            password
-        };
+        const payload = { username, password };
         this.props.handleSubmit(payload);
     };
 
-    protected handleChange = (name: string) => (event: InputChangeEvent) => {
+    protected handleChange = () => (event: InputChangeEvent) => {
+        const { name, value } = event.target;
         this.setState({
             ...this.state,
-            [name]: event.target.value
+            [name]: value
         });
     };
 
     public render() {
-        const {classes} = this.props;
+        const { classes } = this.props;
 
         return (
-            <React.Fragment>
-                <Typography variant="title" color="inherit" noWrap>
-                    <FormattedMessage id={'word.login.title'} />
-                </Typography>
-                <form className={classes.container} noValidate autoComplete="off" onSubmit={this.handleSubmit}
-                      id="LoginForm">
-                    <TextField
-                        required
-                        id="login-email"
-                        label={<FormattedMessage id={'email.label'} />}
-                        name="username"
-                        onChange={this.handleChange('username')}
-                        type="email"
-                        defaultValue=""
-                        className={classes.textField}
-                        margin="normal"
-                        fullWidth
-                        InputLabelProps={{
-                            shrink: true
-                        }}
-                        InputProps={{
-                            className: classes.input
-                        }}
-                    />
+            <form className={ classes.container } noValidate autoComplete="off" onSubmit={ this.handleSubmit }
+                  id="LoginForm">
+                <SprykerInput
+                    isRequired
+                    label={ <FormattedMessage id={ 'email.label' } /> }
+                    inputName="username"
+                    onChangeHandler={ this.handleChange() }
+                    inputType="email"
+                    inputValue={ this.state.username }
+                />
 
-                    <TextField
-                        required
-                        id="login-password"
-                        label={<FormattedMessage id={'word.password.title'} />}
-                        name="password"
-                        onChange={this.handleChange('password')}
-                        type="password"
-                        defaultValue=""
-                        className={classes.textField}
-                        margin="normal"
-                        fullWidth
-                        InputLabelProps={{
-                            shrink: true
-                        }}
-                        InputProps={{
-                            className: classes.input
-                        }}
-                    />
+                <SprykerInput
+                    isRequired
+                    label={ <FormattedMessage id={ 'word.password.title' } /> }
+                    inputName="password"
+                    onChangeHandler={ this.handleChange() }
+                    inputType="password"
+                    inputValue={ this.state.password }
+                />
 
-                    <Button type="submit" variant="contained" className={classes.button}>
-                        <FormattedMessage id={'word.login.title'} />
-                    </Button>
-
-                </form>
-            </React.Fragment>
+                <Button fullWidth type="submit" variant="contained">
+                    <FormattedMessage id={ 'word.login.title' } />
+                </Button>
+            </form>
         );
     }
 }
 
-export const LoginForm = withStyles(styles)(LoginFormBase);
+export const LoginForm = withStyles(styles)(LoginFormComponent);
--- src/application/pages/LoginPage/index.tsx
+++ src/application/pages/LoginPage/index.tsx
@@ -7,11 +7,8 @@ import { NavLink } from 'react-router-dom';
 import { withStyles, Grid } from '@material-ui/core';
 import { AppMain } from '@application/components/AppMain';
 import { LoginForm } from './LoginForm';
-import { RegisterFormComponent } from './RegisterFormComponent';
 import { ErrorBoundary } from '@application/hoc/ErrorBoundary';
-import {
-    ILoginPageProps as Props,
-} from './types';
+import { ILoginPageProps as Props } from './types';
 import { styles } from './styles';
 
 @(withRouter as Function)
@@ -25,35 +22,21 @@ export class LoginPageBase extends React.Component<Props> {
     }
 
     public render(): JSX.Element {
-        const {classes, handleSubmitLoginForm, handleSubmitRegisterForm} = this.props;
+        const {classes, handleSubmitLoginForm} = this.props;
 
         return (
             <AppMain>
-                <Grid item xs={12} sm={12} md={6}
-                      direction="column"
-                      container
-                      justify="center"
-                      alignItems="center"
-                >
-                    <ErrorBoundary>
-                        <LoginForm handleSubmit={handleSubmitLoginForm} />
-                    </ErrorBoundary>
-                    <div className={classes.link}>
-                        <NavLink to={pathForgotPassword}>
-                            <FormattedMessage id={'forgot.password.title'} />
-                        </NavLink>
-                    </div>
-                </Grid>
-                <div className={classes.divider} id="divider" />
-                <Grid item xs={12} sm={12} md={6}
-                      direction="column"
-                      container
-                      justify="center"
-                      alignItems="center"
-                >
-                    <ErrorBoundary>
-                        <RegisterFormComponent handleSubmit={handleSubmitRegisterForm} />
-                    </ErrorBoundary>
+                <Grid container justify="center">
+                    <Grid item xs={12} sm={12} md={6}>
+                        <ErrorBoundary>
+                            <LoginForm handleSubmit={handleSubmitLoginForm} />
+                        </ErrorBoundary>
+                        <div className={classes.link}>
+                            <NavLink to={pathForgotPassword}>
+                                <FormattedMessage id={'forgot.password.title'} />
+                            </NavLink>
+                        </div>
+                    </Grid>
                 </Grid>
             </AppMain>
         );
