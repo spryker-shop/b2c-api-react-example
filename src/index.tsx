d826df3249e5a9757a909ee2daba319069a3c97d
Aleksey Belan <alexey.belan@spryker.com>
2019-03-18 15:41:34 +0200

    Glue-6170: Fixed profile routes

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import 'core-js/es6/set';
 import 'raf/polyfill';
 import * as React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider, Store } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { ScrollToTopRoute } from '@application/hoc/ScrollToTopRoute';
@@ -12,21 +13,25 @@ import { sprykerTheme } from './theme/sprykerTheme';
 import { configureStore } from '@stores/configureStore';
 import createHistory from 'history/createBrowserHistory';
 import PageContent from '@application/containers/PageContent';
+import config from './configs/server';
+import { Route } from 'react-router';
 
 const history = createHistory();
 const store: Store<any> = configureStore(history);
 
 export const App = () => (
+    <BrowserRouter>
     <Provider store={store}>
         <ConnectedRouter history={history}>
             <ScrollToTopRoute>
                 <MuiThemeProvider theme={sprykerTheme}>
                     <CssBaseline/>
-                    <PageContent />
+                    <Route path={config.WEB_PATH} component={PageContent}/>
                 </MuiThemeProvider>
             </ScrollToTopRoute>
         </ConnectedRouter>
     </Provider>
+    </BrowserRouter>
 );
 
 render(<App/>, document.getElementById('app'));
