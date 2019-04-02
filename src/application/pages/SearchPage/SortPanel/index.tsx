bce56d061ad4b8910abfdc0612903c168ca2fb95
Aleksey Belan <alexey.belan@spryker.com>
2019-04-02 15:04:14 +0300

    Glue: 6069 - Styled sort panel mobile resolution.

--- src/application/pages/SearchPage/SortPanel/index.tsx
+++ src/application/pages/SearchPage/SortPanel/index.tsx
@@ -3,7 +3,7 @@ import { connect } from './connect';
 import { FormattedMessage, FormattedPlural } from 'react-intl';
 import { ISortPanelProps as Props, ISortPanelState as State } from './types';
 import { SprykerSelect } from '@application/components/UI/SprykerSelect';
-import { Grid, Typography, withStyles } from '@material-ui/core';
+import { Grid, Hidden, Typography, withStyles } from '@material-ui/core';
 import { styles } from './styles';
 
 @connect
@@ -59,60 +59,64 @@ class SortPanelComponent extends React.Component<Props, State> {
         }
 
         return (
-            <Grid container alignItems="center" className={ classes.root }>
-                <Grid item xs={ 12 } sm={ 3 }>
-                    <Typography color="textSecondary" component="span" variant="subheading">
-                        { pagination.numFound
-                            ? [
-                                `${pagination.numFound} `,
-                                <FormattedPlural
-                                    value={ pagination.numFound }
-                                    key="formatted-text"
-                                    one={ <FormattedMessage id={ 'search.page.one.found.items' } /> }
-                                    other={ <FormattedMessage id={ 'search.page.multiple.found.items' } /> }
-                                />
-                            ]
-                            : <FormattedMessage id={ 'no.found.message' } />
-                        }
-                    </Typography>
-                </Grid>
+            <div className={ classes.root }>
+                <Grid container alignItems="center" spacing={ 24 }>
+                    <Grid item xs={ 6 } sm={ 3 }>
+                        <Typography color="textSecondary" component="span" variant="subheading">
+                            { pagination.numFound
+                                ? [
+                                    `${pagination.numFound} `,
+                                    <FormattedPlural
+                                        value={ pagination.numFound }
+                                        key="formatted-text"
+                                        one={ <FormattedMessage id={ 'search.page.one.found.items' } /> }
+                                        other={ <FormattedMessage id={ 'search.page.multiple.found.items' } /> }
+                                    />
+                                ]
+                                : <FormattedMessage id={ 'no.found.message' } />
+                            }
+                        </Typography>
+                    </Grid>
 
-                <Grid item xs={ 12 } sm={ 9 }>
-                    <Grid container spacing={ 24 } className={ classes.sortsOuter }>
-                        <Grid item xs={ 4 } md={ 5 }>
-                            { sortParams.length &&
-                                <SprykerSelect
-                                    currentMode={ currentSort ? currentSort : ' ' }
-                                    changeHandler={ this.handleSetSorting }
-                                    menuItems={ this.sortMenuItems() }
-                                    menuItemFirst={{
-                                        value: ' ',
-                                        name: <FormattedMessage id={ 'relevance.sort.model.title' } />,
-                                        disabled: !(this.props.sortParams.length > 0)
-                                    }}
-                                    name="sort"
-                                    classes={{ menu: classes.relevanceSortMenu }}
-                                />
-                            }
-                        </Grid>
-                        <Grid item>
-                            { pagination.validItemsPerPageOptions &&
-                                <SprykerSelect
-                                    currentMode={ currentItemsPerPage }
-                                    changeHandler={ this.handleSetItemsPerPage }
-                                    menuItems={ this.itemsPerPageMenuItems() }
-                                    menuItemFirst={{
-                                        value: ' ',
-                                        name: <FormattedMessage id={ 'products.per.page.title' } />,
-                                        disabled: true
-                                    }}
-                                    name="pages"
-                                />
-                            }
+                    <Grid item xs={ 6 } sm={ 9 }>
+                        <Grid container spacing={ 24 } className={ classes.sortsOuter }>
+                            <Grid item xs={ 12 } sm={ 5 }>
+                                { sortParams.length &&
+                                    <SprykerSelect
+                                        currentMode={ currentSort ? currentSort : ' ' }
+                                        changeHandler={ this.handleSetSorting }
+                                        menuItems={ this.sortMenuItems() }
+                                        menuItemFirst={{
+                                            value: ' ',
+                                            name: <FormattedMessage id={ 'relevance.sort.model.title' } />,
+                                            disabled: !(this.props.sortParams.length > 0)
+                                        }}
+                                        name="sort"
+                                        classes={{ menu: classes.relevanceSortMenu }}
+                                    />
+                                }
+                            </Grid>
+                            <Hidden only={['xs']}>
+                                <Grid item className={ classes.sortsAmount }>
+                                    { pagination.validItemsPerPageOptions &&
+                                        <SprykerSelect
+                                            currentMode={ currentItemsPerPage }
+                                            changeHandler={ this.handleSetItemsPerPage }
+                                            menuItems={ this.itemsPerPageMenuItems() }
+                                            menuItemFirst={{
+                                                value: ' ',
+                                                name: <FormattedMessage id={ 'products.per.page.title' } />,
+                                                disabled: true
+                                            }}
+                                            name="pages"
+                                        />
+                                    }
+                                </Grid>
+                            </Hidden>
                         </Grid>
                     </Grid>
                 </Grid>
-            </Grid>
+            </div>
         );
     };
 }
--- src/application/pages/SearchPage/SortPanel/styles.tsx
+++ src/application/pages/SearchPage/SortPanel/styles.tsx
@@ -3,13 +3,22 @@ import createStyles from '@material-ui/core/styles/createStyles';
 
 export const styles = (theme: Theme) => createStyles({
     root: {
-        paddingBottom: 30,
+        paddingBottom: 20,
+        [theme.breakpoints.up('md')]: {
+            paddingBottom: 30
+        },
         [theme.breakpoints.up('lg')]: {
             paddingBottom: 20
         }
     },
     sortsOuter: {
         justifyContent: 'flex-end'
+    },
+    sortsAmount: {
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block'
+        }
     },
     relevanceSortMenu: {
         minWidth: 270
