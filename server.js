a92d7552c65c5dc08b174d55303830c33578f0d2
Aleksey Belan <alexey.belan@spryker.com>
2019-04-02 15:38:16 +0300

    Glue: 4384 - Deleted express

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-const config = require('./server-config');
+const webpack = require( 'webpack' );
+const WebpackDevServer = require( 'webpack-dev-server' );
 
-let devServer;
-let webServer;
+const config = require( './server-config' );
+const webpackConfig = require( './webpack.config' );
 
 if (config.webpackDevServer) {
-    devServer = require('./dev-server');
-} else {
-    webServer = require('./web-server');
+    new WebpackDevServer( webpack( webpackConfig ), {
+        publicPath: webpackConfig.output.publicPath,
+        hot: true,
+        historyApiFallback: true,
+    } ).listen( config.DEV_SERVER_PORT, config.DEV_SERVER_HOST, function (error, result) {
+        if (error) {
+            console.error( error );
+        }
+        console.info( 'Running Webpack Dev Server at ' + config.DEV_SERVER_HOST + ':' + config.DEV_SERVER_PORT );
+    } );
 }
-
-module.exports = {
-    devServer,
-    webServer
-};
