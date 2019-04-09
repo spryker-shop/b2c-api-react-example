50d8d7bc3d26b1c1cb6c1c8fbaf7482334f09fc6
Aleksey Belan <alexey.belan@spryker.com>
2019-04-09 13:22:43 +0300

    Glue: 7010 - Fixed blurring content after expand "view more" button

--- src/application/containers/ContentCollapser/styles.ts
+++ src/application/containers/ContentCollapser/styles.ts
@@ -2,7 +2,7 @@ import { Theme } from '@material-ui/core/styles/createMuiTheme';
 import createStyles from '@material-ui/core/styles/createStyles';
 
 export const styles = (theme: Theme) => createStyles({
-    content: {
+    contentLimited: {
         overflow: 'hidden',
         position: 'relative',
         '&:after': {
