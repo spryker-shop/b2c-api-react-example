eb874118cc2ca38226e1b1cd2b3912f591c2c47b
Aleksey Belan <alexey.belan@spryker.com>
2019-04-22 12:53:35 +0300

    Glue: 6353 - PR fixes

--- src/application/components/UI/SprykerInput/InputMask/index.tsx
+++ src/application/components/UI/SprykerInput/InputMask/index.tsx
@@ -3,18 +3,17 @@ import { IInputMaskProps as Props } from './types';
 import NumberFormat from 'react-number-format';
 
 export const InputMask: React.SFC<Props> = (props): JSX.Element => {
-    const { inputRef, onChange, ...other } = props;
-    const { name } = other;
+    const { inputRef, onChange, ...inputOtherProps } = props;
 
     return (
         <NumberFormat
-            {...other}
+            {...inputOtherProps}
             getInputRef={inputRef}
             onValueChange={values => {
                 onChange({
                     target: {
                         value: values.value,
-                        name
+                        name: inputOtherProps.name
                     },
                 });
             }}
