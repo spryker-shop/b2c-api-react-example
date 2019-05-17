8c1b8fb0534d6518171c4219241a88285a88e580
Aleksey Belan <alexey.belan@spryker.com>
2019-05-17 13:35:11 +0300

    Glue-7040: Reverted hot reload

--- src/application/containers/PageContent/index.tsx
+++ src/application/containers/PageContent/index.tsx
@@ -137,4 +137,4 @@ class PageContentComponent extends React.Component<Props, State> {
 }
 
 const PageContent = withStyles(styles)(PageContentComponent);
-export default PageContent;
+export default hot(PageContent);
