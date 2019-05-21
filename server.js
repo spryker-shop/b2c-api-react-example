64eb8f630e5f104f25c7feabab3d357ed6b9033c
Aleksey Belan <alexey.belan@spryker.com>
2019-05-21 12:04:00 +0300

    Glue-7611: Changed server configs

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
 const config = require('./env_config');
 
-let devServer;
-let webServer;
+const webPath = config.WEB_PATH;
+const webServer = express();
+const appRouter = express.Router();
 
-if (!config.IS_PRODUCTION) {
-    devServer = require('./dev-server');
-} else {
-    webServer = require('./web-server');
-}
+webServer.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
-module.exports = {
-    devServer,
-    webServer
-};
+appRouter.use(express.static(path.join(__dirname, 'build/web')));
+
+appRouter.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build/web/index.html'));
+});
+
+webServer.use(webPath, appRouter);
+
+webServer.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send(err.message);
+});
+
+webServer.set('port', config.WEB_PORT);
+
+const server = http.createServer(webServer);
+
+server.listen(config.WEB_PORT, () => console.info(`Running Web Server at localhost:${config.WEB_PORT}`));
+
+module.exports = server;
--- web-server.js
+++ web-server.js
@@ -1,38 +1,26 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-
 const config = require('./env_config');
 
-let webPath = config.WEB_PATH;
-
-// This feature for Docker container
-try {
-    webPath = JSON.parse(webPath);
-} catch (e) {
-
-}
-
+const webPath = config.WEB_PATH;
 const webServer = express();
+const appRouter = express.Router();
+
 webServer.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-const appRouter = express.Router();
 
-// Point static path to dist
 appRouter.use(express.static(path.join(__dirname, 'build/web')));
 
-// Catch all other routes and return the client app
 appRouter.use('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build/web/index.html'));
 });
 
 webServer.use(webPath, appRouter);
 
-
-// Error Handler
 webServer.use((err, req, res, next) => {
     console.error(err);
     res.status(500).send(err.message);
@@ -40,14 +28,8 @@ webServer.use((err, req, res, next) => {
 
 webServer.set('port', config.WEB_PORT);
 
-/**
- * Create HTTP server.
- */
 const server = http.createServer(webServer);
 
-/**
- * Listen on provided port, on all network interfaces.
- */
 server.listen(config.WEB_PORT, () => console.info(`Running Web Server at localhost:${config.WEB_PORT}`));
 
 module.exports = server;
