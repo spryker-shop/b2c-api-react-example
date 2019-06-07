e4716d363d303f4771b7487597b7528d572e98a8
Aleksey Belan <alexey.belan@spryker.com>
2019-06-07 16:18:40 +0300

    Glue-7546: Fixed transform helpers.

--- src/helpers/common/transform.ts
+++ src/helpers/common/transform.ts
@@ -2,7 +2,7 @@ import { TRangesType } from '@interfaces/search';
 import { rangeMaxType, rangeMinType } from '@constants/search';
 
 export const rangeFilterValueToFront = (value: number, type: TRangesType): number => {
-    const valueFixed = (value / 100);
+    const valueFixed: number = value / 100;
 
     if (type === rangeMinType) {
         return Math.floor(valueFixed);
