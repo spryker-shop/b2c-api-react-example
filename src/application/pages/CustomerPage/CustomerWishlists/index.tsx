f1bb714fb795be5867c9a001bc4cf55ccf36546b
Aleksey Belan <alexey.belan@spryker.com>
2019-06-13 17:19:46 +0300

    Glue-7536: Changed CustomerWishlistsComponent to the FC

--- src/application/pages/CustomerPage/CustomerWishlists/index.tsx
+++ src/application/pages/CustomerPage/CustomerWishlists/index.tsx
@@ -8,29 +8,26 @@ import { WishlistsList } from './WishlistsList';
 import { Typography, withStyles } from '@material-ui/core';
 import { styles } from './styles';
 
-@connect
-class CustomerWishlistsComponent extends React.Component<Props> {
-    public render() {
-        const { classes } = this.props;
+const CustomerWishlistsComponent: React.FC<Props> = (props): JSX.Element => {
+    const { classes } = props;
 
-        return (
-            <>
-                <Typography component="h1" variant="h2" className={ classes.title }>
-                    <FormattedMessage id={ 'word.wishlist.title' } />
-                </Typography>
+    return (
+        <>
+            <Typography component="h1" variant="h2" className={ classes.title }>
+                <FormattedMessage id={ 'word.wishlist.title' } />
+            </Typography>
 
-                <div className={ classes.block }>
-                    <ErrorBoundary>
-                        <AddNewWishlistForm />
-                    </ErrorBoundary>
+            <div className={ classes.block }>
+                <ErrorBoundary>
+                    <AddNewWishlistForm />
+                </ErrorBoundary>
 
-                    <ErrorBoundary>
-                        <WishlistsList />
-                    </ErrorBoundary>
-                </div>
-            </>
-        );
-    }
-}
+                <ErrorBoundary>
+                    <WishlistsList />
+                </ErrorBoundary>
+            </div>
+        </>
+    );
+};
 
-export const CustomerWishlists = withStyles(styles)(CustomerWishlistsComponent);
+export const CustomerWishlists = connect(withStyles(styles)(CustomerWishlistsComponent));
